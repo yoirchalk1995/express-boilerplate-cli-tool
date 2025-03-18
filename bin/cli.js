db824b5d1dba558786fef3799e49c9ff7895ed63
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-03-18 20:05:55 +0200

    catch error if incorrect type passed

--- bin/cli.js
+++ bin/cli.js
@@ -3,16 +3,28 @@
 import { program } from "commander";
 import chalk from "chalk";
 
+const typeOptions = ["model", "route"];
+
 program
   .version("1.0.0")
-  .description("test cli input")
-  .command("test <name>")
-  .description("display the input")
-  .action((name) => {
-    // Log the input with chalk to add some color to the text
-    console.log(chalk.bgCyan(`You entered: ${name}`));
+  .description(
+    "create file containing boilerplate code for an express route or mongoose model"
+  )
+  .argument("<type>", `type of file to create; ${typeOptions}`)
+  .argument("<name>", "name of file to contain boilerplate code")
+  .option("-a, --async", "specify if route handler is async", false)
+  .action((type, name, options) => {
+    if (!typeOptions.includes(type)) {
+      console.log(chalk.red("type not recognized."));
+    }
 
-    process.exit(0);
+    if (type === "model") {
+      console.log(`creating model with name ${name}`);
+    }
+    if (type === "route") {
+      console.log(
+        `creating route with name ${name}. isAsync: ${options.async}`
+      );
+    }
   });
-
 program.parse(process.argv);
