bc80e95d2db1676d3353b27b5d3423064a2952ce
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-03-20 21:29:14 +0200

    updated cli.js to include functionality for -c flag. tested and seems to be working

--- bin/cli.js
+++ bin/cli.js
@@ -15,29 +15,42 @@ program
   .argument("<type>", `type of file to create; ${typeOptions}`)
   .argument(
     "[name]",
-    "Name of file to contain boilerplate code. Required unless-c flag is passed"
+    "Name of file to contain boilerplate code. Required unless -c flag is passed"
   )
   .option("-a, --async", "specify if route handler is async", false)
-  .option("-c, --custom <file>", "specify file to act as custom template for ")
+  .option("-c, --custom [file]", "specify file to act as custom template for ")
   .action((type, name, options) => {
     const { async: isAsync, custom } = options;
     if (custom) {
-      console.log(custom);
-      return;
+      if (name) {
+        console.log(chalk.red("name is not compatible with --custom flag"));
+        process.exit();
+      }
+      if (custom === true) {
+        console.log(chalk.red("custom boilerplate code file not provided."));
+        process.exit();
+      }
+      customBoileplate(custom);
     }
-
     if (!typeOptions.includes(type)) {
-      console.log(chalk.red("type not recognized."));
+      console.log(chalk.red(`<type> must be one of [${typeOptions}]`));
     }
     if (type === "model") {
       modelFunction(name);
+      process.exit();
     }
     if (type === "route") {
-      routerFunction(name, isAsync);
+      routerFunction(name);
+      process.exit();
     }
     if (type === "both") {
-      routerFunction(name, isAsync);
+      routerFunction(name);
       modelFunction(name);
+      process.exit();
     }
   });
 program.parse(process.argv);
+
+function customBoileplate(type) {
+  console.log(type);
+}
