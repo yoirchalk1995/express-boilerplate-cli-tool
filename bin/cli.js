cffdbb6df741d636643617faad5973e515dbd123
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-03-22 22:38:07 +0200

    updated version in commander app

--- bin/cli.js
+++ bin/cli.js
@@ -9,7 +9,7 @@ import customFunction from "../lib/customBoilerplateFunction.js";
 const typeOptions = ["model", "route", "both"];
 
 program
-  .version("0.1.0-beta.1")
+  .version("1.2.0")
   .description(
     "create file containing boilerplate code for an express route and or or mongoose model"
   )
